30e5240578a42c607c755c4645247775daa51214
johnkntran <john.kn.tran@gmail.com>
2025-10-21 05:13:47 -0400

    Serve simple HTTP server on backend.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
-import time
 import logging
+import http.server
+import socketserver
+
+HOST = '0.0.0.0'
+PORT = 8000
 
 logger = logging.getLogger(__name__)
+Handler = http.server.SimpleHTTPRequestHandler
 
-logger.warning('** Sleeping for a week **')
-time.sleep(604800)
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    logger.warning(f'** Serving at: http://{HOST}:{PORT}/ **')
+    httpd.serve_forever()
